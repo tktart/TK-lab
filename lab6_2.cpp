d9f1f24af7f1c8319fd641cdab37044ad291886b
tktart <tonkow235@gmail.com>
2024-12-03 01:15:15 +0700

    ii

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
+double deg2rad(double a1){
+    return M_PI*a1/180;
+}
+double rad2deg(double a2){
+    return 180/M_PI*a2;
+}
+double findXComponent(double l1,double l2,double a1,double a2){
+    return l1*cos(a1)+l2*cos(a2);
+}
+double findYComponent(double l1,double l2,double a1,double a2){
+    return l1*sin(a1)+l2*sin(a2);
+}
+double pythagoras(double xcomp,double ycomp){
+    return sqrt(pow(xcomp,2)+pow(ycomp,2));
+}
+void showResult(double result_vec_length,double result_vec_direction){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+    cout << "Length of the resultant vector = "<< result_vec_length <<"\n";
+    cout << "Direction of the resultant vector (deg) = "<< result_vec_direction<<"\n";
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n";
+}
+
 int main(){
-    //sss
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
     cout << "Enter length of the first vector: ";
     cin >> l1;
