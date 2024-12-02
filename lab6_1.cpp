c65449fd85e25e485d51b2537dc67f7b1ee56e95
tktart <tonkow235@gmail.com>
2024-12-03 00:20:11 +0700

    eiei

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main(){
+    int x, e=0, o=0;
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> x;
+    while(x != 0){
+        if (x % 2 == 0){
+            e += 1;
+        }else{
+            o += 1;
+        }
+        cout << "Enter an integer: ";
+        cin >> x;
+    }
+    cout << "#Even numbers = "<< e<< "\n";
+    cout << "#Odd numbers = "<< o << "\n";
     return 0;
 }
